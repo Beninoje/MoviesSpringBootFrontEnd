962e9d95a328c066bf320f8de75cdebf89e498a6
Beni <beninoje@hotmail.com>
2025-03-21 20:39:24 -0400

    changed API directory

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,3 +1,3 @@
-export const BASE_API_URL=`http://localhost:8080/api/v1`
+export const BASE_API_URL=`http://movies-springboot-api-env.eba-bus5biay.us-east-2.elasticbeanstalk.com/api/v1`
 // http://movies-springboot-api-env.eba-bus5biay.us-east-2.elasticbeanstalk.com
 // http://localhost:8080
